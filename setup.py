0d5570f1f5717efbdd743e0a1b1a2e7d1c5eed3e
cr21 <cr.tagadiya@gmail.com>
2022-12-29 20:19:40 -0500

    removed and refactored

--- setup.py
+++ setup.py
@@ -1,40 +1,43 @@
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 from typing import List
 
-#Declaring variables for setup functions
-PROJECT_NAME = "finance-complaint"
-VERSION="0.0.3"
-AUTHOR="chirag Tagadiya"
-AUTHOR_EMAIL="cr.tagadiya@gmail.com"
-DESRCIPTION="Consumer Complaint Finance classification"
 
-REQUIREMENT_FILE_NAME="requirements.txt"
+# Declaring variables for setup functions
+PROJECT_NAME = "src"
+VERSION = "0.0.1"
+AUTHOR = "Chirag Tagadiya"
+AUTHOR_EMAIL = "cr.tagadiya@gmail.com"
+DESRCIPTION = "finance consumer complaint classification"
+
+REQUIREMENT_FILE_NAME = "requirements.txt"
 
 HYPHEN_E_DOT = "-e ."
 
 
-def get_requirements_list() -> List[str]:
+def get_all_requirement_list() -> List[str]:
     """
-    Description: This function is going to return list of requirement
-    mention in requirements.txt file
-    return This function is going to return a list which contain name
-    of libraries mentioned in requirements.txt file
+    Get All Requirements from reading from requirements.txt
+    :return: List of requirements
+    :rtype: List[str]
     """
-    with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        requirement_list = requirement_file.readlines()
-        requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+
+    with open(REQUIREMENT_FILE_NAME) as req_files:
+        requirement_list: List[str] = []
+        all_raw_requirements = req_files.readlines()
+        requirement_list = [req_name.replace("\n", "") for req_name in all_raw_requirements]
+
         if HYPHEN_E_DOT in requirement_list:
             requirement_list.remove(HYPHEN_E_DOT)
+
         return requirement_list
 
 
-
 setup(
-name=PROJECT_NAME,
-version=VERSION,
-author=AUTHOR,
-author_email=AUTHOR_EMAIL,
-description=DESRCIPTION,
-packages=find_packages(), 
-install_requires=get_requirements_list()
+    name=PROJECT_NAME,
+    version=VERSION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    description=DESRCIPTION,
+    packages=find_packages(),
+    install_requires=get_all_requirement_list()
 )
